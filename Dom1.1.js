720e3811ee053788c845d5887fb68b5ef6f4b310
JerryOliva <consultaesthetic@gmail.com>
2023-10-10 11:00:33 -0600

    objects-constructorMethod "constructor class and objects"

---
+++ Dom1.1.js
@@ -0,0 +1,4 @@
+const parrafo1=document.querySelector("#p1");
+parrafo1.textContent=("Despues de todo estamos haciendo posible día a día nuestra misión en esta tierra,confía");
+const parrafo2=document.querySelector("#p2")
+parrafo2.innerHTML=("This comedy is an <em>ilusion</em>")
